20a0c1eb0a5daba1662feee13727e21658e72ca0
noahthomlison <noahthomlison@gmail.com>
2022-02-03 10:50:47 -0800

    refactors the filterbar component

--- my-app/src/Components/FilterBar.js
+++ my-app/src/Components/FilterBar.js
@@ -1,11 +1,11 @@
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 function FilterBar() {
   return (
-    <Container>
-      <Typography>Shop by</Typography>
+    <Container maxWidth={"xl"}>
+      <Typography variant="h4">Shop by</Typography>
       <Button variant="contained">Instrument</Button>
       <Button variant="contained">Availabiltiy</Button>
       <Button variant="contained">Location</Button>
@@ -15,4 +15,3 @@ function FilterBar() {
 }
 
 export default FilterBar;
- 
