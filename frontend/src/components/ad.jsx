891457a3f38924d2cf14a4d12399f3b1c3ed41e8
Tyler <tylershelton99@gmail.com>
2022-02-04 15:25:06 -0500

    Add functionality to the ad component that will display all the information about the listing that was clicked on.

--- frontend/src/components/ad.jsx
+++ frontend/src/components/ad.jsx
@@ -1,8 +1,100 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ListingsDataService from '../services/listings';
+import { Link, useParams } from 'react-router-dom';
 
-function Ad(props) {
+const Ad = props => {
   const { userName } = props;
-  return <div className='App'>Ad Component for {userName} </div>;
-}
+  const id = useParams().id;
+  const initialListingState = {
+    id: null,
+    title: '',
+    description: '',
+    instrument_type: '',
+    brand: '',
+    condition: '',
+    price: {},
+    security_deposit: 0,
+    images: [],
+    host_id: '',
+    host: {},
+    address: {},
+  };
+
+  const [listing, setListing] = useState(initialListingState);
+
+  const getListing = id => {
+    ListingsDataService.get(id)
+      .then(response => {
+        setListing(response.data.listing[0]);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  useEffect(() => {
+    getListing(id);
+  }, [id]);
+
+  return (
+    <div>
+      {listing ? (
+        <div className='text-center'>
+          <h5>{listing.title}</h5>
+          <br />
+          <img src={listing.images[0]} />
+          <br />
+          <br />
+          <p>
+            <strong>Description: </strong>
+            <br />
+            {listing.description}
+            <br />
+            <strong>Instrument Type: </strong>
+            {listing.instrument_type}
+            <br />
+            <strong>Brand: </strong>
+            {listing.brand}
+            <br />
+            <strong>Condition: </strong>
+            {listing.condition}
+            <br />
+            <strong>Price: </strong>
+            <br />
+            <strong>Daily:</strong> ${listing.price.daily}
+            <br />
+            <strong>Weekly:</strong> ${listing.price.weekly}
+            <br />
+            <strong>Monthly:</strong> ${listing.price.monthly}
+            <br />
+            <strong>Security Deposit Required: </strong>
+            {listing.security_deposit}
+            <br />
+            <strong>Location: </strong>
+            {listing.address.city}, {listing.address.province}
+          </p>
+          <br />
+          <hr />
+          <p>
+            <strong>Hosted By: </strong>
+            <br />
+            <br />
+            <img className='rounded img-fluid' src={listing.host.image} />
+            <br />
+            <br />
+            <strong>{listing.host.name}</strong>
+            <br />
+            <em>{listing.host.about}</em>
+          </p>
+        </div>
+      ) : (
+        <div>
+          <br />
+          <p>No Listing Found.</p>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Ad;
