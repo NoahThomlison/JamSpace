1570a347d43b3dc3bfd8c4f17a995ae491d17269
Tyler <tylershelton99@gmail.com>
2022-02-03 19:05:41 -0500

    Add the ability to retrive the listing information from the backend database and display on the temporary base page.  Implement the functionality to filter by select options

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import Custom Components
+import AdListings from './components/ad-listings';
+import Login from './components/login';
+import Ad from './components/ad';
+import CreateListing from './components/create-listing';
 
 function App() {
+  const [user, setUser] = useState('Tyler');
+
+  async function login(user = null) {
+    setUser(user);
+  }
+
+  async function logout() {
+    setUser(null);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div>
+      <nav className='navbar navbar-expand navbar-dark bg-dark'>
+        <a href='/' className='navbar-brand'>
+          JamSpace
         </a>
-      </header>
+        <div className='navbar-nav mr-auto'>
+          <li className='nav-item'>
+            <Link to={'/listings/new'} className='nav-link'>
+              New Listing
+            </Link>
+          </li>
+          <li className='nav-item'>
+            {user ? (
+              <a
+                onClick={logout}
+                href='/login'
+                className='nav-link'
+                style={{ cursor: 'pointer' }}
+              >
+                Logout {user.name}
+              </a>
+            ) : (
+              <Link to={'/login'} className='nav-link'>
+                Login
+              </Link>
+            )}
+          </li>
+        </div>
+      </nav>
+
+      <div className='container mt-3'>
+        <Routes>
+          <Route path={'/'} element={<AdListings />} />
+          <Route path='/listings/new' element={<CreateListing user={user} />} />
+          <Route path='/listings/:id' element={<Ad userName={user} />} />
+          <Route path='/login' element={<Login login={login} />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- frontend/src/components/ad-listings.jsx
+++ frontend/src/components/ad-listings.jsx
@@ -1,7 +1,206 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ListingsDataService from '../services/listings';
+import { Link } from 'react-router-dom';
 
-function AdListings() {
-  return <div className='App'>Ad Listings Component</div>;
-}
+const AdListings = props => {
+  const [listings, setListings] = useState([]);
+  const [searchBrand, setSearchBrand] = useState('');
+  const [searchCity, setSearchCity] = useState('');
+  const [searchDescription, setSearchDescription] = useState('');
+  const [searchInstrumentType, setSearchInstrumentType] = useState('');
+
+  useEffect(() => {
+    retrieveListings();
+  }, []);
+
+  const onChangeSearchBrand = e => {
+    const brand = e.target.value;
+    setSearchBrand(brand);
+  };
+
+  const onChangeSearchCity = e => {
+    const city = e.target.value;
+    setSearchCity(city);
+  };
+
+  const onChangeSearchDescription = e => {
+    const description = e.target.value;
+    setSearchDescription(description);
+  };
+
+  const onChangeSearchInstrumentType = e => {
+    const instrumentType = e.target.value;
+    setSearchInstrumentType(instrumentType);
+  };
+
+  const retrieveListings = () => {
+    ListingsDataService.getAll()
+      .then(response => {
+        console.log('HEEERRRRRREEEEE');
+        console.log(response);
+        setListings(response.data.listings);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
+  const refreshList = () => {
+    retrieveListings();
+  };
+
+  const find = (query, by) => {
+    ListingsDataService.find(query, by)
+      .then(response => {
+        console.log(response.data);
+        setListings(response.data.listings);
+      })
+      .catch(e => {
+        console.error(e);
+      });
+  };
+
+  const findByBrand = () => {
+    find(searchBrand, 'brand');
+  };
+
+  // This is not working yet.
+  const findByCity = () => {
+    find(searchCity, 'city');
+  };
+
+  const findByDescription = () => {
+    find(searchDescription, 'description');
+  };
+
+  const findByInstrumentType = () => {
+    find(searchInstrumentType, 'instrument_type');
+  };
+
+  // Sample Test Code
+  //const address = 'Hamilton, Ontario, Canada';
+
+  console.log(listings);
+
+  return (
+    <div>
+      <div className='row pb-1'>
+        <div className='input-group col-lg-4'>
+          <input
+            type='text'
+            className='form-control'
+            placeholder='Search by Brand'
+            value={searchBrand}
+            onChange={onChangeSearchBrand}
+          />
+          <div className='input-group-append'>
+            <button
+              className='btn btn-outline-secondary'
+              type='button'
+              onClick={findByBrand}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+        <div className='input-group col-lg-4'>
+          <input
+            type='text'
+            className='form-control'
+            placeholder='Search by City'
+            value={searchCity}
+            onChange={onChangeSearchCity}
+          />
+          <div className='input-group-append'>
+            <button
+              className='btn btn-outline-secondary'
+              type='button'
+              onClick={findByCity}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+        <div className='input-group col-lg-4'>
+          <input
+            type='text'
+            className='form-control'
+            placeholder='Search by Keyword'
+            value={searchDescription}
+            onChange={onChangeSearchDescription}
+          />
+          <div className='input-group-append'>
+            <button
+              className='btn btn-outline-secondary'
+              type='button'
+              onClick={findByDescription}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+        <div className='input-group col-lg-4'>
+          <input
+            type='text'
+            className='form-control'
+            placeholder='Search by Instrument Type'
+            value={searchInstrumentType}
+            onChange={onChangeSearchInstrumentType}
+          />
+          <div className='input-group-append'>
+            <button
+              className='btn btn-outline-secondary'
+              type='button'
+              onClick={findByInstrumentType}
+            >
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className='row'>
+        {listings.map(listing => {
+          const address = `${listing.address.city}, ${listing.address.province}, ${listing.address.country}`;
+          return (
+            <div className='col-lg-4 pb-1'>
+              <div className='card'>
+                <div className='card-body'>
+                  <h5 className='card-title'>{listing.title}</h5>
+                  <p className='card-text'>
+                    {listing.description}
+                    <br />
+                    <strong>Instrument Type: </strong>
+                    {listing.instrument_type}
+                    <br />
+                    <strong>Brand: </strong>
+                    {listing.brand}
+                    <br />
+                    <strong>Address: </strong>
+                    {address}
+                  </p>
+                  <div className='row'>
+                    <Link
+                      to={'/listings/' + listing._id}
+                      className='btn btn-primary col-lg-5 mx-1 mb-1'
+                    >
+                      View Ad
+                    </Link>
+                    <a
+                      target='_blank'
+                      href={'https://www.google.com/maps/place/' + address}
+                      className='btn btn-primary col-lg-5 mx-1 mb-1'
+                    >
+                      View Map
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default AdListings;
