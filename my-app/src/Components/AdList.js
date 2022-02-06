1e7c3bf97f5593f4304cfb32a701e8a19eba0daa
noahthomlison <noahthomlison@gmail.com>
2022-02-06 12:45:34 -0800

    changes key to id={ad._is.$oid} to send proper id to ads

--- my-app/src/Components/AdList.js
+++ my-app/src/Components/AdList.js
@@ -36,21 +36,19 @@ function Adlist(props) {
  
   return (
     <Container sx={{display:"flex", justifyContents:"center", alignItems:"center"}}>
-      {min/adCount > 0 ? 
+          {min/adCount > 0 ? 
       <IconButton onClick={() => {previous()}}><ArrowBackIosIcon color="primary" /></IconButton > : 
       <IconButton ><ArrowBackIosIcon color="disabled"/></IconButton >}
-
       <ImageList cols={6} rows={2}>
         {ads.map((ad) => {
           return (
-            <Ad key={ad.id} images={ad.images} price={ad.price} description = {ad.description} title = {ad.title}/>
+            <Ad id={ad._id.$oid} images={ad.images} price={ad.price} description = {ad.description} title = {ad.title}/>
           )
         })}
       </ImageList>
       {max/adCount >= slideCount ? 
       <IconButton ><ArrowForwardIosIcon color="disabled"/></IconButton > : 
       <IconButton onClick={() => next()}><ArrowForwardIosIcon color="primary"/></IconButton >}
-
     </Container>
   )
 }
