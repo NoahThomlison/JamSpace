82ea52bde7da923d1e9afaa1894780ca7bbeef2f
noahthomlison <noahthomlison@gmail.com>
2022-02-06 11:44:26 -0800

    refactors home to box

--- my-app/src/Components/Ad.js
+++ my-app/src/Components/Ad.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Card, CardActions, CardContent, CardMedia, CardHeader, Button, Grid, Typography} from '@mui/material/';
+import {Card, CardActions, CardContent, CardMedia, CardHeader, Button, Grid, Typography, Container} from '@mui/material/';
 
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
@@ -7,60 +7,21 @@ import ImageListItemBar from '@mui/material/ImageListItemBar';
 import ListSubheader from '@mui/material/ListSubheader';
 import IconButton from '@mui/material/IconButton';
 import InfoIcon from '@mui/icons-material/Info';
+import { makeStyles } from '@mui/styles';
 
-function Ad({description, title, price, images}) {
+function Ad({id, description, title, price, images, key}) {
   const image = images[0]
   const daily = price.daily.$numberInt
   const weekly = price.weekly.$numberInt
   const monthly = price.monthly.$numberInt
   const subheader = "D:$" + daily + "W:$" + weekly + "M:$" +monthly
   
-  // return (
-  //   <Grid item >
-  //     <Card sx={{ maxWidth: 345 }}>
-  //       <CardMedia
-  //         component="img"
-  //         height="250"
-  //         image="https://picsum.photos/id/1025/200/175.jpg"
-  //       />
-  //       <CardContent>
-  //         <CardHeader
-  //         title={title}
-  //         subheader={subheader}
-  //         />
-  //         <Typography variant="body2" color="text.secondary">
-  //           {description}
-  //         </Typography>
-  //       </CardContent>
-  //     </Card>
-  //   </Grid>
-  // );
-
+  // const style = 
   return(
-    <ImageList sx={{ width: 500, height: 450 }}>
-      <ImageListItem key="Subheader" cols={6}>
+      <ImageListItem key={key} onClick={() => alert(id)}>
+        <img src={image} loading="lazy" alt={title} id={id} sx={{height:500}}/>
+        <ImageListItemBar title={title} subtitle={subheader}/>
       </ImageListItem>
-        <ImageListItem key="https://picsum.photos/id/1025/200/175.jpg">
-          <img
-            src={"https://picsum.photos/id/1025/200/175.jpg?w=248&fit=crop&auto=format"}
-            srcSet={"https://picsum.photos/id/1025/200/175.jpg?w=248&fit=crop&auto=format"}
-            alt={title}
-            loading="lazy"
-          />
-          <ImageListItemBar
-            title={title}
-            subtitle="Noah"
-            actionIcon={
-              <IconButton
-                sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                aria-label={`info about ${title}`}
-              >
-                <InfoIcon />
-              </IconButton>
-            }
-          />
-        </ImageListItem>
-    </ImageList>
   )
 }
 
--- my-app/src/Components/AdList.js
+++ my-app/src/Components/AdList.js
@@ -1,18 +1,56 @@
 import Ad from './Ad.js';
-import {Container, Grid, Item} from '@mui/material/';
+import {Box, Container, Grid, Item, ImageList } from '@mui/material/';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import {useState, useEffect} from "react"
+
+
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
+
 
 function Adlist(props) {
-  const ads = props.data
-  console.log(ads)
+
+  let adCount = 6
+  let slideCount = Math.ceil(props.data.length/adCount)
+  const [min, setMin] = useState(0)
+  const [max, setMax] = useState(adCount)
+  const [ads, setAds] = useState(props.data.slice(min, max))
+
+  useEffect(() => {
+    setAds(props.data.slice(min, max));
+  }, [min, max, props.data]);
+
+  const next = () => {
+    setMin(min+adCount)
+    setMax(max+adCount)
+  }
+
+  const previous = () => {
+    setMin(min-adCount)
+    setMax(max-adCount)
+  }
+ 
   return (
-    <Container>
-      <Grid container spacing={2}>
+    <Container sx={{display:"flex", justifyContents:"center", alignItems:"center"}}>
+      {min/adCount > 0 ? 
+      <IconButton onClick={() => {previous()}}><ArrowBackIosIcon color="primary" /></IconButton > : 
+      <IconButton ><ArrowBackIosIcon color="disabled"/></IconButton >}
+
+      <ImageList cols={6} rows={2}>
         {ads.map((ad) => {
           return (
-            <Ad images={ad.images} price={ad.price} description = {ad.description} title = {ad.title}/>
+            <Ad key={ad.id} images={ad.images} price={ad.price} description = {ad.description} title = {ad.title}/>
           )
         })}
-      </Grid>
+      </ImageList>
+      {max/adCount >= slideCount ? 
+      <IconButton ><ArrowForwardIosIcon color="disabled"/></IconButton > : 
+      <IconButton onClick={() => next()}><ArrowForwardIosIcon color="primary"/></IconButton >}
+
     </Container>
   )
 }
