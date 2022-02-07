d26824aef4b17e2617e54eb6566f9344e1f5050d
noahthomlison <noahthomlison@gmail.com>
2022-02-07 06:09:15 -0800

    adds in navlink to individual ad, removes unrequired imports

--- frontend/src/components/Ad.js
+++ frontend/src/components/Ad.js
@@ -1,27 +1,23 @@
 import * as React from 'react';
-import {Card, CardActions, CardContent, CardMedia, CardHeader, Button, Grid, Typography, Container} from '@mui/material/';
-import '../App.css';
-
-import ImageList from '@mui/material/ImageList';
+import { NavLink } from 'react-router-dom';
 import ImageListItem from '@mui/material/ImageListItem';
 import ImageListItemBar from '@mui/material/ImageListItemBar';
-import ListSubheader from '@mui/material/ListSubheader';
-import IconButton from '@mui/material/IconButton';
-import InfoIcon from '@mui/icons-material/Info';
 
-function Ad({id, description, title, price, images, key}) {
+function Ad({ad, id, description, title, price, images, key}) {
   console.log({price})
   const image = images[0]
   const daily = price.daily
   const weekly = price.weekly
   const monthly = price.monthly
   const subheader = "D:$" + daily + "W:$" + weekly + "M:$" +monthly
-
+ 
   return(
+    <NavLink to={`/listings/${id}`} className='nav-link'>
         <ImageListItem key={key}>
-          <img src={image} loading="lazy" alt={title} id={id} sx={{height:"100%", width:"auto"}}/>
+        <img src={image} loading="lazy" id={id} sx={{height:"100%",width:"auto"}}/>
         <ImageListItemBar title={title} subtitle={subheader}/>
       </ImageListItem>
+    </NavLink>
   )
 }
 
