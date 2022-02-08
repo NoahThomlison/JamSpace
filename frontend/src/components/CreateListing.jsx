2a3a8c180b5bbbea59d386d6698892e58e1b20d5
noahthomlison <noahthomlison@gmail.com>
2022-02-07 21:45:02 -0800

    refactors simple textfields into drop down and multilines, begins to add in drag adn drop components:

--- frontend/src/components/CreateListing.jsx
+++ frontend/src/components/CreateListing.jsx
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import ListingsDataService from '../services/listings';
 import { Link } from 'react-router-dom';
 
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import Select from '@mui/material/Select';
+
+import {useDropzone} from 'react-dropzone'
+
+
 //MUI
-import { Autocomplete, TextField } from "@mui/material"
+import { Autocomplete, Box, TextField, FormControl, InputLabel, OutlinedInput, InputAdornment } from "@mui/material"
 
 
 const CreateListing = props => {
@@ -91,9 +98,16 @@ const CreateListing = props => {
     }
   };
 
+  const {acceptedFiles, getRootProps, getInputProps} = useDropzone();
+  
+  const files = acceptedFiles.map(file => (
+    <li key={file.path}>
+      {file.path} - {file.size} bytes
+    </li>
+  ));
+
   return (
     <div>
-
       {/* If the username is not set as an empty string, display the new listing form, otherwise display the message to Login */}
       {username !== '' ? (
         <div className='submit-form mb-5'>
@@ -106,17 +120,97 @@ const CreateListing = props => {
             </div>
           ) : (
             <div>
+
+
+
+
               <h3 className='mb-4'>{editing ? 'Edit' : 'Create'} Listing: </h3>
-                <TextField id="outlined-basic" label="Title" name="title" variant="outlined" onChange={handleInputChange}/>
-                <TextField id="outlined-basic" label="Description" name="description" variant="outlined"  onChange={handleInputChange}/>
-                <TextField id="outlined-basic" name="instrument_type" label="Instument Type" variant="outlined"/>
-                <TextField id="outlined-basic" name="brand" label="Brand" variant="outlined" onChange={handleInputChange}/>
-                <TextField id="outlined-basic" name="condition"  label="Condition" variant="outlined" onChange={handleInputChange}/>
-                <TextField id="outlined-basic" name="images" label="Link to Image" variant="outlined" onChange={handleInputChange}/> 
-                <TextField id="outlined-basic" name='daily' label="Daily:" variant="outlined" onChange={handleInputChange}/>
-                <TextField id="outlined-basic" name='weekly' label="Weekly:" variant="outlined" onChange={handleInputChange}/>
-                <TextField id="outlined-basic" name='monthly' label="Monthly:" variant="outlined" onChange={handleInputChange}/>
-                <TextField id="outlined-basic" name='deposit' label="Security Deposit:" variant="outlined" onChange={handleInputChange}/>
+
+                <Box className="container" sx={{backgroundColor: "#808080"}}>
+      <div {...getRootProps({className: 'dropzone'})}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some files here, or click to select files</p>
+      </div>
+      <aside>
+        <h4>Files</h4>
+        <ul>{files}</ul>
+      </aside>
+    </Box>
+
+    <TextField id="outlined-basic" label="Title" name="title" variant="outlined" onChange={handleInputChange}/>
+
+    
+                {/* <TextField id="outlined-textarea" label="Description" name="description" variant="outlined"  onChange={handleInputChange}/> */}
+                <TextField
+                  id="outlined-multiline-static"
+                  label="Description"
+                  name="description"
+                  multiline
+                  rows={4}
+                  onChange={handleInputChange}
+                />
+                {/* dropdown */}
+                {/* <TextField id="outlined-basic" name="instrument_type" label="Instument Type" variant="outlined"/> */}
+                <FormControl sx={{width:150}}>
+                  <InputLabel id="demo-simple-select-helper-label">Instument Type</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-helper-label"
+                    id="demo-simple-select-helper"
+                    name="instrument_type"
+                    label="Brand"
+                    onChange={handleInputChange}
+                  >
+                    <MenuItem value={10}>Guitar</MenuItem>
+                    <MenuItem value={20}>Drums</MenuItem>
+                    <MenuItem value={30}>Soemthing else?</MenuItem>
+                  </Select>
+                </FormControl>
+
+
+                {/* <TextField id="outlined-basic" name="condition"  label="Condition" variant="outlined" onChange={handleInputChange}/> */}
+                <FormControl sx={{width:150}}>
+                  <InputLabel id="demo-simple-select-helper-label">Condition</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-helper-label"
+                    id="demo-simple-select-helper"
+                    name="condition"
+                    label="Condition"
+                    onChange={handleInputChange}
+                  >
+                    <MenuItem value={10}>Excellent</MenuItem>
+                    <MenuItem value={20}>Good</MenuItem>
+                    <MenuItem value={30}>Fair</MenuItem>
+                    <MenuItem value={30}>Not so hot</MenuItem>
+                    <MenuItem value={30}>Smashed</MenuItem>
+                  </Select>
+                </FormControl>
+
+
+                {/* <TextField id="outlined-basic" name="brand" label="Brand" variant="outlined" onChange={handleInputChange}/> */}
+                <FormControl sx={{width:150}}>
+                  <InputLabel id="demo-simple-select-helper-label">Brand</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-helper-label"
+                    id="demo-simple-select-helper"
+                    name="brand"
+                    label="Brand"
+                    onChange={handleInputChange}
+                  >
+                    <MenuItem value={10}>Something</MenuItem>
+                    <MenuItem value={20}>Something</MenuItem>
+                    <MenuItem value={30}>Something</MenuItem>
+                  </Select>
+                </FormControl>
+
+                <TextField id="outlined-basic" name="images" label="Link to Image" variant="outlined" onChange={handleInputChange}/>
+
+                {/* //prices  */}
+                <TextField id="outlined-basic" name='daily' label="Daily($)" variant="outlined" onChange={handleInputChange}/>
+                <TextField id="outlined-basic" name='weekly' label="Weekly($)" variant="outlined" onChange={handleInputChange}/>
+                <TextField id="outlined-basic" name='monthly' label="Monthly($)" variant="outlined" onChange={handleInputChange}/>
+                <TextField id="outlined-basic" name='deposit' label="Security Deposit($)" variant="outlined" onChange={handleInputChange}/>
+
+                {/* //location  */}
                 <TextField id="outlined-basic" name='city' label="City:" variant="outlined" onChange={handleInputChange}/>
                 <TextField id="outlined-basic" name='province' label="Province:" variant="outlined" onChange={handleInputChange}/>
                 <TextField id="outlined-basic" name='country' label="Country:" variant="outlined" onChange={handleInputChange}/>
@@ -149,3 +243,5 @@ const CreateListing = props => {
 };
 
 export default CreateListing;
+
+
