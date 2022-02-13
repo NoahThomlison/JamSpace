15a0b841d91d6bf0a59e5db71cba0b169b9a8172
fostlt <lanceadamp@gmail.com>
2022-02-13 13:16:56 -0800

    update home page to master

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,8 @@
+
 // Import React Component
 import React from 'react';
-
 // Import Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 // Import fadeIn
 import FadeInSection from "../helpers/fadeIn"
 
@@ -12,18 +11,24 @@ import AdListings from '../components/AdListings';
 import Questions from '../components/Questions';
 import AdList from '../components/AdList';
 import About from './About'
+import Filters from './Filters'
 import Map from './Map';
+
 
 // Import Images
 import heroImage from '../images/thehitsBWDark.jpg';
 import logo from '../images/logoWhite.png';
 import store from '../images/store1.jpg'
+import drums from '../images/Drums.jpg'
+import guitar1 from '../images/Guitars.jpg'
+import guitar2 from '../images/Guitars2.jpg'
+import guitar3 from '../images/Guitars3.jpg'
 import map from '../images/amps_drums.jpg'
 
+
 // Import MUI
-import { Box } from '@mui/material/';
+import { Box, Paper } from '@mui/material/';
 import { makeStyles } from '@mui/styles';
-import TabScrollButton from '@mui/material/TabScrollButton';
 
 const useStyles = makeStyles({
   hero: {
@@ -35,18 +40,26 @@ const useStyles = makeStyles({
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    objectFit: "contains"
   },
   logo: {
     maxWidth: "15%",
     height: "auto",
-    },
-  store:{
+  },
+  store: {
     backgroundImage: `url(${store})`,
     height: "100vh",
     display: "flex",
     backgroundSize: 'auto',
+    backgroundSize: 'cover',
     backgroundRepeat: 'no-repeat',
+    justifyContent: "center",
+    alignItems: "center",   
+  },
+  listings:{
+    backgroundImage: `url(${guitar2})`,
+    height: "100vh",
+    display: "flex",
+    backgroundSize: 'cover',
     justifyContent: "center",
     alignItems: "center",  
   },
@@ -60,7 +73,6 @@ const useStyles = makeStyles({
     justifyContent: "center",
     alignItems: "center",  
   }
-
 });
 
 const Home = props => {
@@ -68,26 +80,37 @@ const Home = props => {
   const styles = useStyles();
 
   return (
-    <div sx={{color: "#FF0000"}}>
-      <Box className={styles.main}>
-        <Box className={styles.hero}>
+    <Box >
+      {/* View 1 */}
+      <Box className={styles.hero}>
             <img src={logo} className={styles.logo}></img>
         </Box>
-        <Box className={styles.store}>
+      {/* View 2 */}
+      <Box className={styles.listings}>
           <FadeInSection>
-            <About />
-            <Questions />
+            <Paper>
+            <Filters setListings={setListings}></Filters>
+            <AdList listings={listings}></AdList>
+            </Paper>
           </FadeInSection>
         </Box>
-        <FadeInSection>
-          <AdList listings={listings}></AdList>
-        </FadeInSection>
+
+      {/* View 3 */}
+
+      <Box className={styles.store}>
+
+      <FadeInSection>
+      <Paper>
+              <About />
+              <Questions />
+            </Paper>
+          </FadeInSection>
+        </Box>
         <Box className={styles.map}>
           <Map listings={listings}/>
         </Box>
-        
-      </Box>
-    </div>
+
+    </Box>
   );
 };
 
