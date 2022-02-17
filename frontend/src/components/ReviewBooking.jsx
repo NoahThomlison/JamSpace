33fce4ea41b51638ff67ed74729c51af08cccc0f
noahthomlison <noahthomlison@gmail.com>
2022-02-17 09:05:32 -0800

    updates styling to match individual ad, adds backgrounds

--- frontend/src/components/ReviewBooking.jsx
+++ frontend/src/components/ReviewBooking.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
 import { Container, Paper, Typography, Box, TextField, Button } from "@mui/material"
+import { useNavigate } from 'react-router-dom';
 
 
 const ReviewBooking = props => {
@@ -10,21 +11,27 @@ const ReviewBooking = props => {
   const firstDay = booking.minDate.toLocaleDateString('en-US', options);
   const lastDay = booking.maxDate.toLocaleDateString('en-US', options);
   const { numOfDays, rentalRate, rental, deposit, total } = booking;
+  const navigate = useNavigate();
+
+  const navigateToAd = (listing) => {
+    console.log(listing)
+    navigate('/listings/' + listing._id)
+  }
 
   return (
     <Container>
       <Paper sx={{display:"flex", flexDirection: "column"}}>
         <Typography variant="h5">Review Your Order</Typography>
-        <TextField className={props.styles.input} value={`${firstDay}`} InputLabelProps={{shrink: true}} InputProps={{readOnly: true}} label='First Day' variant='outlined'/>
-        <TextField className={props.styles.input} value={`${lastDay}`} InputLabelProps={{shrink: true}} InputProps={{readOnly: true}} label='Last Day' variant='outlined'/>
-        <TextField className={props.styles.input} value={`${numOfDays}`} InputLabelProps={{shrink: true}} InputProps={{readOnly: true}} label='Number of Days' variant='outlined'/>
-        <TextField className={props.styles.input} value={`$${rentalRate}`} InputLabelProps={{shrink: true}} InputProps={{readOnly: true}} label='Rental Rate' variant='outlined'/>
-        <TextField className={props.styles.input} value={`$${rental}`} InputLabelProps={{shrink: true}} InputProps={{readOnly: true}} label='Rental Cost' variant='outlined'/>
-        <TextField className={props.styles.input} value={`$${deposit}`} InputLabelProps={{shrink: true}} InputProps={{readOnly: true}} label='Security Deposit' variant='outlined'/>
-        <TextField className={props.styles.input} value={`$${total}`} InputLabelProps={{shrink: true}} InputProps={{readOnly: true}} label='Total' variant='outlined'/>
+        <TextField sx={{marginTop: "10px"}} className={props.styles.input} value={`${firstDay}`} InputLabelProps={{shrink: true}} InputProps={{readOnly: true}} label='First Day' variant='outlined'/>
+        <TextField sx={{marginTop: "10px"}} className={props.styles.input} value={`${lastDay}`} InputLabelProps={{shrink: true}} InputProps={{readOnly: true}} label='Last Day' variant='outlined'/>
+        <TextField sx={{marginTop: "10px"}} className={props.styles.input} value={`${numOfDays}`} InputLabelProps={{shrink: true}} InputProps={{readOnly: true}} label='Number of Days' variant='outlined'/>
+        <TextField sx={{marginTop: "10px"}} className={props.styles.input} value={`$${rentalRate}`} InputLabelProps={{shrink: true}} InputProps={{readOnly: true}} label='Rental Rate' variant='outlined'/>
+        <TextField sx={{marginTop: "10px"}} className={props.styles.input} value={`$${rental}`} InputLabelProps={{shrink: true}} InputProps={{readOnly: true}} label='Rental Cost' variant='outlined'/>
+        <TextField sx={{marginTop: "10px"}} className={props.styles.input} value={`$${deposit}`} InputLabelProps={{shrink: true}} InputProps={{readOnly: true}} label='Security Deposit' variant='outlined'/>
+        <TextField sx={{marginTop: "10px"}} className={props.styles.input} value={`$${total}`} InputLabelProps={{shrink: true}} InputProps={{readOnly: true}} label='Total' variant='outlined'/>
         <Box sx={{display:"flex", justifyContent: "space-around"}}>
           <Button className={props.styles.input} variant="contained" sx={{width:"100%"}} onClick={() => props.next()}>Proceed to Payment</Button>
-          <Button className={props.styles.input} variant="contained" color="error" sx={{width: "100%"}} onClick={() => props.back()}>Back</Button>
+          <Button className={props.styles.input} variant="contained" color="error" sx={{width: "100%"}} onClick={() => navigateToAd(props.listing)}>Back</Button>
         </Box>
       </Paper>
     </Container>
